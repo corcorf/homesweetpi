b0c0f824d839905b2303457ae128081623b6fedf
Flann Corcoran <flann@posteo.net>
2020-03-24 14:41:41 +0100

    add requirements.txt, data_retrieval.service and setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# check to see if this has been done already
+if [ ! -e data_retrieval_service_set_up_complete ]
+then
+  # set up logging as a service
+  echo "copying logger.service to  /etc/systemd/system/"
+  cp data_retrieval.service /etc/systemd/system/data_retrieval.service
+  echo "reloading systemd daemon and enabling service"
+  systemctl daemon-reload
+  systemctl start data_retrieval.service
+  systemctl enable data_retrieval.service
+  echo "creating file 'data_retrieval_service_set_up_complete as flag"
+  touch data_retrieval_service_set_up_complete
+fi
