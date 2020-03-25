77a7616a09102517a6afae1a8fb141761a8d0dae
Flann Corcoran <flann@posteo.net>
2020-03-25 14:44:39 +0100

    clean-up on sql_tables.py

--- homesweetpi/sql_tables.py
+++ homesweetpi/sql_tables.py
@@ -8,7 +8,6 @@ from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
-# from config import HSP_PASSWORD as PASSWORD
 
 HOST = 'localhost'
 PORT = '5432'
@@ -16,7 +15,6 @@ USERNAME = 'homesweetpi'
 PASSWORD = os.getenv('HSP_PASSWORD')
 DB = 'homesweetpi'
 conn_string = f'postgres://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DB}'
-# CONN_STRING = 'sqlite:///{}'.format(DB_PATH)
 Base = declarative_base()
 ENGINE = create_engine(conn_string, echo=False)
 Session = sessionmaker(bind=ENGINE)
@@ -109,89 +107,86 @@ class Measurement(Base):
         return data
 
 
-def create_tables(engine):
+def create_tables(engine=ENGINE):
     """
-    Creates all tables in the sql database
+    Create all tables in the sql database
     """
     logging.debug(f'Creating tables in sql')
     Base.metadata.create_all(engine)
 
 
-def get_pi_names():
+def get_pi_names(session=Session()):
     """
     Return an array of unique raspberry pi names
     """
-    session = Session()
     q = session.query(RaspberryPi)
     result = session.query(distinct(q.c.name)).all()
     result = np.array(result).reshape(-1).astype(str)
     return result
 
 
-def get_pi_ips():
+def get_pi_ips(session=Session()):
     """
     Return an array of unique raspberry pi ip addresses
     """
-    session = Session()
     q = session.query(RaspberryPi)
     result = session.query(distinct(q.c.ipaddress)).all()
     result = np.array(result).reshape(-1).astype(str)
     return result
 
 
-def get_pi_names_and_addresses():
-    """Return a list of name, ip-address tuples"""
-    session = Session()
+def get_pi_names_and_addresses(session=Session()):
+    """
+    Return a list of name, ip-address tuples
+    """
     return session.query(RaspberryPi.name, RaspberryPi.ipaddress).all()
 
 
-def get_pi_ids():
-    """Return a list of pi id numbers"""
-    session = Session()
+def get_pi_ids(session=Session()):
+    """
+    Return a list of pi id numbers
+    """
     return np.array(session.query(RaspberryPi.id).all()).flatten()
 
 
-def load_sensor_and_pi_info(pi_file, sensor_file):
+def load_sensor_and_pi_info(pi_file, sensor_file, engine=ENGINE):
     """
     Upload info on pis and sensors to the db
     """
     pis = pd.read_csv(pi_file)
-    pis.to_sql("raspberrypis", ENGINE, index=False, if_exists="append")
+    pis.to_sql("raspberrypis", engine, index=False, if_exists="append")
 
     sensors = pd.read_csv(sensor_file)
     sensors.drop(['name'], axis=1, inplace=True)
-    sensors.to_sql("sensors", ENGINE, index=False, if_exists="append")
+    sensors.to_sql("sensors", engine, index=False, if_exists="append")
 
 
-def get_all_sensors():
+def get_all_sensors(session=Session()):
     """
     Return an array of unique raspberry pi names
     """
-    session = Session()
     q = session.query(Sensor)
     result = session.query(distinct(q.c.name)).all()
     result = np.array(result).reshape(-1).astype(str)
     return result
 
 
-def get_sensors_on_pi(piid):
+def get_sensors_on_pi(piid, session=Session()):
     """
     Get a dataframe relating location names and pi name to sensor id for
     a given pi
     """
-    session = Session()
     q = session.query(Sensor).join(RaspberryPi).filter(RaspberryPi.id == piid)
     sensors = q.values("sensors.id", "location", "name")
     return pd.DataFrame(sensors).rename(columns={"name": "piname",
                                                  "sensors.id": "sensorid"})
 
 
-def get_last_time(piid):
+def get_last_time(piid, session=Session()):
     """
     Get the time of the most recent reading for a given raspberry pi
     Returns a datetime
     """
-    session = Session()
     q = session.query(Measurement).join(Sensor).join(RaspberryPi)
     q = q.filter(Sensor.piid == piid).order_by(Measurement.datetime.desc())
     result = q.first()
@@ -201,12 +196,11 @@ def get_last_time(piid):
         return datetime(1970, 1, 1)
 
 
-def get_ip_addr(piid):
+def get_ip_addr(piid, session=Session()):
     """
     Query the SQL database to find the ipaddress for a given pi_id
     Returns a string with the ip address
     """
-    session = Session()
     q = session.query(RaspberryPi).filter(RaspberryPi.id == piid)
     return q.first().ipaddress
 
