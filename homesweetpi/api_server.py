68be8617a73520782afd57c887590fd85f265d4e
Flann Corcoran <flann@posteo.net>
2020-03-26 14:23:27 +0100

    add api route for most recent readings

--- homesweetpi/api_server.py
+++ homesweetpi/api_server.py
@@ -5,21 +5,24 @@ adapted from:
 https://www.codementor.io/@sagaragarwal94/building-a-basic-restful-api-in-python-58k02xsiq
 """
 
-import pandas as pd
 import logging
-import json
 from flask import Flask
 from flask import render_template, request
 from flask_restful import Resource, Api
-from sqlalchemy.orm import sessionmaker
-from homesweetpi.sql_tables import get_all_sensors, get_last_measurement_for_sensor
-from homesweetpi.data_preparation import rewrite_chart, recent_readings_as_html
+from homesweetpi.data_preparation import rewrite_chart,\
+                                         recent_readings_as_html,\
+                                         get_most_recent_readings
 
 app = Flask("homesweetpi")
 api = Api(app)
 
 # Session = sessionmaker(bind=ENGINE)
 logging.basicConfig(filename='homesweetpi_api.log', level=logging.DEBUG)
+
+
+class GetLast(Resource):
+    def get(self):
+        return get_most_recent_readings()
 
 
 @app.route('/')
@@ -49,6 +52,8 @@ def charts():
     )
     return render_template('charts.html',  **context)
 
+
+api.add_resource(GetLast, '/get_last')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5002', debug=False)
